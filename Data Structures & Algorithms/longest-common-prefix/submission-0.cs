123e80dd0ae530d1160b631c2e95adc00930566c
Adulla Shreya Reddy <142567147+shreyaadulla30@users.noreply.github.com>
2026-04-07 14:07:24 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if(strs.Length==0 ||strs==null)
+            return "";
+        for(int i=0;i<strs[0].Length;i++){
+            char c= strs[0][i];
+            for(int j=1;j<strs.Length;j++){
+                if(i>=strs[j].Length|| strs[j][i]!= c){
+                    return strs[0].Substring(0,i);
+                }
+            }
+        }
+       return strs[0];
+    }
+}
