0a23169e450afed50def40e05c518e292d18c2a4
Adulla Shreya Reddy <142567147+shreyaadulla30@users.noreply.github.com>
2026-04-08 15:52:53 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int n= nums.Length;
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+            
+        }
+        return k;
+    }
+}
