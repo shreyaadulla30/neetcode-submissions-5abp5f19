1cc293a110f9b4b0a91cff5c80b1f3ff69e0d6a5
Adulla Shreya Reddy <142567147+shreyaadulla30@users.noreply.github.com>
2026-04-10 15:31:12 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0;i<nums.Length-1;i++){
+            for(int j=i+1;j<nums.Length;j++){
+                if(nums[i]==nums[j]&& Math.Abs(i-j)<=k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
